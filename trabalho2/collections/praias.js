67c186b9198c37f5f44a4ee2fb9243086f40a21f
Beatriz Cavalheri <106353730+biacavalheri@users.noreply.github.com>
2025-10-29 22:34:26 -0300

    Create praias.js

---
+++ trabalho2/collections/praias.js
@@ -0,0 +1,123 @@
+// Coleção: praias
+db.praias.insertMany([
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9021"),
+    cod_praia: 111,
+    nome: "Praia do Gonzaga",
+    localizacao: {
+      latitude: -23.967773,
+      longitude: -46.332611
+    },
+    cidade: "Santos",
+    estado: "SP",
+    total_armarios: 2
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9022"),
+    cod_praia: 112,
+    nome: "Praia do Boqueirão",
+    localizacao: {
+      latitude: -23.979722,
+      longitude: -46.370833
+    },
+    cidade: "Santos",
+    estado: "SP",
+    total_armarios: 2
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9023"),
+    cod_praia: 113,
+    nome: "Praia do José Menino",
+    localizacao: {
+      latitude: -23.991389,
+      longitude: -46.380556
+    },
+    cidade: "Santos",
+    estado: "SP",
+    total_armarios: 2
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9024"),
+    cod_praia: 211,
+    nome: "Praia do Guaraú",
+    localizacao: {
+      latitude: -24.308056,
+      longitude: -46.995000
+    },
+    cidade: "Peruíbe",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9025"),
+    cod_praia: 311,
+    nome: "Praia Grande",
+    localizacao: {
+      latitude: -23.433056,
+      longitude: -45.071944
+    },
+    cidade: "Ubatuba",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9026"),
+    cod_praia: 312,
+    nome: "Praia do Tenório",
+    localizacao: {
+      latitude: -23.441389,
+      longitude: -45.063056
+    },
+    cidade: "Ubatuba",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9027"),
+    cod_praia: 411,
+    nome: "Praia de Maresias",
+    localizacao: {
+      latitude: -23.793889,
+      longitude: -45.565000
+    },
+    cidade: "São Sebastião",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9028"),
+    cod_praia: 412,
+    nome: "Praia da Baleia",
+    localizacao: {
+      latitude: -23.806944,
+      longitude: -45.558611
+    },
+    cidade: "São Sebastião",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9029"),
+    cod_praia: 511,
+    nome: "Praia Martim de Sá",
+    localizacao: {
+      latitude: -23.633889,
+      longitude: -45.413611
+    },
+    cidade: "Caraguatatuba",
+    estado: "SP",
+    total_armarios: 1
+  },
+  {
+    _id: ObjectId("661a1b2c3d4e5f6a7b8c9030"),
+    cod_praia: 611,
+    nome: "Praia dos Sonhos",
+    localizacao: {
+      latitude: -24.180556,
+      longitude: -46.788611
+    },
+    cidade: "Itanhaém",
+    estado: "SP",
+    total_armarios: 1
+  }
+]);
